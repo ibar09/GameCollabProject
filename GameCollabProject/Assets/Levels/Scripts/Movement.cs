20592fa3321b31880cae379af73af9a22bc3ae13
monta-b <montasbargoum@gmail.com>
2022-06-05 14:33:02 +0100

    same forces problem in both wall jump and dashing

--- GameCollabProject/Assets/Levels/Scripts/Movement.cs
+++ GameCollabProject/Assets/Levels/Scripts/Movement.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody2D body;
+    // jumping and speed 
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
+
+    // layers
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
+
+    //wall detection and jump
     public Transform TopRight;
     public Transform BotttomLeft;
     private bool firstTime = true;// hedhi bsh tkhali l player yjumpi mara barka mn ala wall maghirha player can spam Space and get over the wall
+
+    //dashing
+    [SerializeField]private float dashDistance;
+    private float doubleTapTimer;
+    private KeyCode lastKeyCode;
+
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
@@ -19,6 +30,37 @@ public class Movement : MonoBehaviour
     //badlt barsha fil wall jumping system
     private void Update()
     {
+        //hethi partie taa dashing 
+            //dashing right 
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {   //second time pressing
+            if(doubleTapTimer>Time.time && lastKeyCode==KeyCode.RightArrow)
+            {
+                dash(1);// 1 is for the direction
+            }
+            //first time pressing 
+            else
+            {
+                doubleTapTimer=Time.time+0.4f; // after u press a key 3andk el 7a9 fi 0.5 secs to verify the condition fel if li 9ablha
+            }
+            lastKeyCode=KeyCode.RightArrow;
+        }
+                //dashing left
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {   //second time pressing
+            if(doubleTapTimer>Time.time && lastKeyCode==KeyCode.LeftArrow)
+            {
+                dash(-1);// -1 is for the direction
+            }
+            //first time pressing 
+            else
+            {
+                doubleTapTimer=Time.time+0.4f; 
+            }
+            lastKeyCode=KeyCode.LeftArrow;
+
+
+        }
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded() || onWall()))
         {
             if (onWall() && firstTime)
@@ -32,6 +74,7 @@ public class Movement : MonoBehaviour
                 firstTime = true;
             }
         }
+    
     }
     private void FixedUpdate()
     {
@@ -45,8 +88,8 @@ public class Movement : MonoBehaviour
 
         if (onWall())
         {
+            body.velocity=new Vector2(-10,10);//im trying to make the player bounce of the wall up and in the opposite direction instead of climibing it ( didnt work u try it )
             body.gravityScale = 2;
-            body.velocity = Vector2.up * jumpSpeed;
 
         }
         else if (isGrounded())
@@ -90,6 +133,14 @@ public class Movement : MonoBehaviour
 
         return Physics2D.OverlapArea(TopRight.position, BotttomLeft.position, wallLayer);
 
+    }
+
+    private void dash(int direction)
+    {   
+        
+        body.gravityScale=0;
+        body.velocity=new Vector2(dashDistance*direction,0);//i straight up cant understand how to add a force in a specific direction in unity if u do know tell me 
+        body.gravityScale=2;
     }
 
 }
